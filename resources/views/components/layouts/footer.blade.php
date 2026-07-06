4660a916fc65b5d997add016187e15ab5f40cf1c
Roi <roi.liem98@gmail.com>
2026-07-06 11:45:37 +0900

    switch to desktop env

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,31 +1,37 @@
 <div class=" bg-forest text-white ">
-    <div class="grid grid-cols-1 md:grid-cols-3">
-        <div class="flex flex-col justify-center items-center text-center p-4">
+    <div class="grid grid-cols-1 md:grid-cols-5 p-12">
+        <div class="flex flex-col justify-center items-center text-center p-4 col-span-3">
             <div class="w-full flex flex-col justify-center items-start text-left gap-2">
                 <img src="{{ asset('/ysbh.png') }}" alt="Logo YSBH" class="h-32 my-4">
                 <span>Yayasan Sinar Bhakti Husada</span>
-                <span>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</span>
-                <span>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</span>
+                <span>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</span>
 
-                
+
             </div>
         </div>
-        <div class="">
-            <div class="text-2xl font-bold p-4">
+        <div class="flex flex-col justify-start items-start text-left p-4">
+            <div class="text-2xl font-bold mb-3">
                 Kartu Program
             </div>
+            <div class="flex flex-col justify-start items-start text-left gap-1">
+                <span>Malaria</span>
+                <span>Kesehatan Ibu & Anak</span>
+                <span>Imunisasi</span>
+            </div>
         </div>
-        <div class="">
-            <div class="text-2xl font-bold p-4">
+        <div class="flex flex-col justify-start items-start text-left p-4">
+            <div class="text-2xl font-bold mb-3">
                 Info Kontak
             </div>
-            <div class="">
-                +62 812 3456 7890
-                +62 812 3456 7777
+            <div class="flex flex-col justify-start items-start text-left gap-1">
+                <span>+62 812 3456 7890</span>
+                <span>+62 812 3456 7890</span>
+                <span>Jl. Raya Bogor No. 123, Jakarta</span>
+                <span>contact@ysbh.org</span>
             </div>
         </div>
     </div>
-    <div class="border-t-2 border-terracotta w-full p-2 flex justify-between items-center">
+    <div class="border-t-2 border-terracotta w-full py-4 px-12 flex justify-between items-center">
         <span>Yayasan Sinar Bhakti Husada </span>
         <span>&copy; {{ date('Y') }}.</span>
     </div>
--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -11,7 +11,43 @@
             </button>
             <p class="mt-2 text-lg">Tombol diklik: <span x-text="count" class="font-bold"></span> kali</p>
         </div>
-        
+
+        {{-- <livewire:test-counter /> --}}
+
+        <hr style="margin: 20px 0;">
+
+        <div x-data="{ count: 0 }" style="padding: 20px;">
+            <button @click="count++" style="padding: 10px; background: blue; color: white;">
+                Klik Alpine
+            </button>
+            <p>Tombol diklik: <span x-text="count"></span> kali</p>
+        </div><!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+        landing bitch
+        <div x-data="{ count: 0 }" class="p-5">
+            <button @click="count++" class="px-4 py-2 bg-blue-500 text-white rounded">
+                Klik Saya
+            </button>
+            <p class="mt-2 text-lg">Tombol diklik: <span x-text="count" class="font-bold"></span> kali</p>
+        </div>
+
+        {{-- <livewire:test-counter /> --}}
+
+        <hr style="margin: 20px 0;">
+
+        <div x-data="{ count: 0 }" style="padding: 20px;">
+            <button @click="count++" style="padding: 10px; background: blue; color: white;">
+                Klik Alpine
+            </button>
+            <p>Tombol diklik: <span x-text="count"></span> kali</p>
+        </div><!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+        landing bitch
+        <div x-data="{ count: 0 }" class="p-5">
+            <button @click="count++" class="px-4 py-2 bg-blue-500 text-white rounded">
+                Klik Saya
+            </button>
+            <p class="mt-2 text-lg">Tombol diklik: <span x-text="count" class="font-bold"></span> kali</p>
+        </div>
+
         {{-- <livewire:test-counter /> --}}
 
         <hr style="margin: 20px 0;">
